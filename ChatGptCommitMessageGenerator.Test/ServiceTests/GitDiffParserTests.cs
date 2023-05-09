fae2a2c95891475648644ef5144529fc29d39485
Courtland <Courtland9777@gmail.com>
2023-05-08 23:43:19 -0400

    updating GitDiffParserTests

--- ChatGptCommitMessageGenerator.Test/ServiceTests/GitDiffParserTests.cs
+++ ChatGptCommitMessageGenerator.Test/ServiceTests/GitDiffParserTests.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using ChatGptCommitMessageGenerator.Abstractions;
 using ChatGptCommitMessageGenerator.Services;
 using Xunit;
 
@@ -7,65 +7,74 @@ namespace ChatGptCommitMessageGenerator.Test.ServiceTests
 {
     public class GitDiffParserTests
     {
-        private readonly GitDiffParser _gitDiffParser;
+        private readonly IGitDiffParser _gitDiffParser;
 
         public GitDiffParserTests() => _gitDiffParser = new GitDiffParser();
 
-        public static IEnumerable<object[]> GitDiffTestData =>
-            new List<object[]>
-            {
-                new object[]
-                {
-                    @"@@ -1,3 +1,4 @@
-some code
-+added code
-some more code",
-                    1
-                },
-                new object[] { "", 0 },
-                new object[] { "some text without git diff pattern", 0 },
-                new object[]
-                {
-                    @"@@ -1,3 +1,4 @@
-some code
-+added code
-some more code
-@@ -5,6 +5,7 @@
-more code
-+another added code
-more code again",
-                    2
-                },
-                new object[]
-                {
-                    @"@@ -1,3 +1,5 @@
-some code
-+added code
-+another added code
-some more code",
-                    1
-                },
-                new object[]
-                {
-                    @"@@ -1,4 +1,3 @@
-some code
--removed code
-some more code
-+added code",
-                    1
-                }
-            };
-
-        [Theory]
-        [MemberData(nameof(GitDiffTestData))]
-        public async Task ExtractGitDiffChangesAsync_ValidCases_ReturnsExpectedChangesCountAsync(string gitDiff,
-            int expectedChangesCount)
+        [Fact]
+        public async Task ExtractGitDiffChangesAsync_WithValidGitDiff_ReturnsListOfChangesAsync()
         {
+            // Arrange
+            const string gitDiff = @"
+diff --git a/README.md b/README.md
+index 4f4d3e1..2d5f5b5 100644
+--- a/README.md
++++ b/README.md
+@@ -1,4 +1,4 @@
+-# OldProjectName
++# NewProjectName
+ This is a simple project.
+ 
+@@ -10,4 +10,4 @@
+-This line has been removed.
++This line has been added.
+";
+
+            const int expectedChangesCount = 2;
+
             // Act
-            var changes = await _gitDiffParser.ExtractGitDiffChangesAsync(gitDiff);
+            var result = await _gitDiffParser.ExtractGitDiffChangesAsync(gitDiff);
 
             // Assert
-            Assert.Equal(expectedChangesCount, changes.Count);
+            Assert.Equal(expectedChangesCount, result.Count);
+            Assert.Contains("@@ -1,4 +1,4 @@\r\n", result);
+            Assert.Contains("@@ -10,4 +10,4 @@\r\n", result);
+        }
+
+        [Fact]
+        public async Task ExtractGitDiffChangesAsync_WithEmptyGitDiff_ReturnsEmptyListAsync()
+        {
+            // Arrange
+            var gitDiff = string.Empty;
+
+            // Act
+            var result = await _gitDiffParser.ExtractGitDiffChangesAsync(gitDiff);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task ExtractGitDiffChangesAsync_WithInvalidGitDiff_ReturnsEmptyListAsync()
+        {
+            // Arrange
+            const string gitDiff = @"
+diff --git a/README.md b/README.md
+index 4f4d3e1..2d5f5b5 100644
+--- a/README.md
++++ b/README.md
+-# OldProjectName
++# NewProjectName
+ This is a simple project.
+ 
+-This line has been removed.
++This line has been added.
+";
+            // Act
+            var result = await _gitDiffParser.ExtractGitDiffChangesAsync(gitDiff);
+
+            // Assert
+            Assert.Empty(result);
         }
     }
 }
